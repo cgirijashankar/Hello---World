08abcd6c0abc0b2b7a82301d38ac61c7010c21ee
SHANKAR <SHANKAR@SHANKAR-VAIO>
2016-09-24 20:05:38 +0530

    First change

---
+++ SeleniumTest/src/main/java/SeleniumTest/SeleniumTest/GUITest1.java
@@ -0,0 +1,29 @@
+package SeleniumTest.SeleniumTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GUITest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "F://chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.in");
+		
+		String actualTitle = driver.getTitle();
+		
+		System.out.println("Actual Title is::" + actualTitle);
+		
+		if (actualTitle.equals("Google")) {
+			System.out.println("True");
+		}else {
+			System.out.println("False");
+		}
+		
+		driver.close();
+		
+
+	}
+
+}
